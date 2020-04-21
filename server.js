0239abf987b4038140d857fbedb99d06e57ea4e6
Tuan Huynh <tuanhuynht1998@gmail.com>
2020-04-21 17:05:34 -0400

    React Hello World

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-
+// production build
+// app.use(express.static(path.join(__dirname,'/client/build')));
 
 
 
