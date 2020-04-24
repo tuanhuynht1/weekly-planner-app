d214025a0b7046b02df1e6f0f8ffa29bfa70f1d1
Tuan Huynh <tuanhuynht1998@gmail.com>
2020-04-23 20:34:00 -0400

    minor changes

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const path = require('path');
 const Database = require('./database');
-
 const app = express();
+const pg = new Database('postgresql://postgres:TT__tt7674@localhost:5432/planner');
+// const pg = new Database(process.env.DATABASE_URL); // production build
 
+/* middleware 
+--------------------------------------------------------------------------------------*/
 // production build
 // app.use(express.static(path.join(__dirname,'/client/build')));
+
+/* API 
+--------------------------------------------------------------------------------------*/
+
+// GET all todos '/todos'
+
 
 
 
 const port = process.env.PORT || 5000;
-app.listen(port, async () => {
-    console.log('listening to port:',port);
-    const pg = new Database('postgresql://postgres:TT__tt7674@localhost:5432/perntodo');
-    console.log(await pg.test());
-});
+app.listen(port, async () => console.log('listening to port:',port));
